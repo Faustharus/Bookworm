5c6b56d98cc5ce9a667e732a00d698db33b12fc4
Faustharus <chailloleau-damien@outlook.fr>
2023-12-15 18:12:45 +0100

    Initial Commit

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Damien Chailloleau on 15/12/2023.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
---
+++ Bookworm/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by Damien Chailloleau on 15/12/2023.
+//
+
+import Foundation
